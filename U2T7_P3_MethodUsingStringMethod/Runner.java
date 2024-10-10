fa806a3d868f742c044242e524aef58ac770601a
Chen Zhi Lin <chenzhil@nycstudents.net>
2024-10-10 21:15:29 +0000

    1

--- U2T7_P3_MethodUsingStringMethod/Runner.java
+++ U2T7_P3_MethodUsingStringMethod/Runner.java
@@ -22,26 +22,163 @@ public class Runner {
         // str = methods.funnyString("apples", 1);
         // System.out.println(str);
 
-        String str = methods.halvesReversed("computer");  //even # of letters
-        System.out.println(str);
-        str = methods.halvesReversed("computers");        //odd # of letters
-        System.out.println(str);
-        str = methods.halvesReversed("sky");
-        System.out.println(str);
-        str = methods.halvesReversed("weekend");
-        System.out.println(str);
-        str = methods.halvesReversed("an");
-        System.out.println(str);
-        str = methods.halvesReversed("I");
-        System.out.println(str);
-        str = methods.halvesReversed("see you soon");
-        System.out.println(str);
-        str = methods.halvesReversed("see you later!");
-        System.out.println(str);
+        // String str = methods.halvesReversed("computer");  //even # of letters
+        // System.out.println(str);
+        // str = methods.halvesReversed("computers");        //odd # of letters
+        // System.out.println(str);
+        // str = methods.halvesReversed("sky");
+        // System.out.println(str);
+        // str = methods.halvesReversed("weekend");
+        // System.out.println(str);
+        // str = methods.halvesReversed("an");
+        // System.out.println(str);
+        // str = methods.halvesReversed("I");
+        // System.out.println(str);
+        // str = methods.halvesReversed("see you soon");
+        // System.out.println(str);
+        // str = methods.halvesReversed("see you later!");
+        // System.out.println(str);
 
+        // String str = methods.pigLatin("computer");
+        // System.out.println(str);
+        // str = methods.pigLatin("sky");
+        // System.out.println(str);
+        // str = methods.pigLatin("Brooklyn");
+        // System.out.println(str);
+        // str = methods.pigLatin("weekend");
+        // System.out.println(str);
+        
+        // String str = methods.removeCharacter("Halloween", 5);
+        // System.out.println(str);
+        // str = methods.removeCharacter("Halloween", 0);
+        // System.out.println(str);
+        // str = methods.removeCharacter("Halloween", 8);
+        // System.out.println(str);
+        // str = methods.removeCharacter("Halloween", 9);
+        // System.out.println(str);
+        // str = methods.removeCharacter("Halloween", 20);
+        // System.out.println(str);
+        // str = methods.removeCharacter("Hi friend", 0);
+        // System.out.println(str);
+        // str = methods.removeCharacter("Hi friend", 1);
+        // System.out.println(str);
+        // str = methods.removeCharacter("Hi friend", 2);
+        // System.out.println(str);
+        // str = methods.removeCharacter(" Hi friend", 0);
+        // System.out.println(str);
+        // str = methods.removeCharacter("A", 0);
+        // System.out.println(str);
+        // str = methods.removeCharacter("A ", 0);
+        // System.out.println(str);
+        // str = methods.removeCharacter("A", 1);
+        // System.out.println(str);
+        // str = methods.removeCharacter("A ", 1);
+        // System.out.println(str);
+       
+        // String str = methods.insertAt("ghost", "BOO!", "o");
+        // System.out.println(str);
+        // str = methods.insertAt("ghost", "BOO!", "st");
+        // System.out.println(str);
+        // str = methods.insertAt("ghost", "BOO!", "m");
+        // System.out.println(str);
+        // str = methods.insertAt("spooooky!", "YIKES", "o");
+        // System.out.println(str);
+        // str = methods.insertAt("happy birthday!", "18th", "y");
+        // System.out.println(str);
+        // str = methods.insertAt("happy birthday!", "18th", "birthday");
+        // System.out.println(str);
+        // str = methods.insertAt("happy birthday!", "18th", " ");
+        // System.out.println(str);
+        // str = methods.insertAt("happy birthday!", " 18th", " ");
+        // System.out.println(str);
+        // str = methods.insertAt("happy birthday!", "18th ", "bir");
+        // System.out.println(str);
+        // str = methods.insertAt("happy birthday!", "18th", "!");
+        // System.out.println(str);
+        // str = methods.insertAt("happy birthday!", "18th", "z");
+        // System.out.println(str);
+        // str = methods.insertAt("a", "m", "a");
+        // System.out.println(str);
+        // str = methods.insertAt("a", "m", "p");
+        // System.out.println(str);
+      
+        // String str = methods.endUp("It's Thursday", 3);
+        // System.out.println(str);
+        // str = methods.endUp("hello friend", 3);
+        // System.out.println(str);
+        // str = methods.endUp("hello friend!", 3);
+        // System.out.println(str);
+        // str = methods.endUp("Shhhh! Be quiet", 3);
+        // System.out.println(str);
+        // str = methods.endUp("Yes", 3);
+        // System.out.println(str);
+        // str = methods.endUp("Yess", 3);
+        // System.out.println(str);
+        // str = methods.endUp("Yesss", 3);
+        // System.out.println(str);
+        // str = methods.endUp("no", 3);
+        // System.out.println(str);
+        // str = methods.endUp("no!", 3);
+        // System.out.println(str);
+        // str = methods.endUp("noo!", 3);
+        // System.out.println(str);
+        // str = methods.endUp("noo!!", 3);
+        // System.out.println(str);
+        // str = methods.endUp("ALREADY UPPERCASE", 3);
+        // System.out.println(str);
+        // str = methods.endUp("hello friend", 1);
+        // System.out.println(str);
+        // str = methods.endUp("hello friend", 6);
+        // System.out.println(str);
+        // str = methods.endUp("hello friend", 20);
+        // System.out.println(str);
+        
+        // String str = methods.yellOrWhisper("Hello James!");
+        // System.out.println(str);
+        // str = methods.yellOrWhisper("hello James!");
+        // System.out.println(str);
+        // str = methods.yellOrWhisper("aBCDEFGHIJK");
+        // System.out.println(str);
+        // str = methods.yellOrWhisper("Abcdefghijk");
+        // System.out.println(str);
+        // str = methods.yellOrWhisper("B");
+        // System.out.println(str);
+        // str = methods.yellOrWhisper("b");
+        // System.out.println(str);
+        // str = methods.yellOrWhisper("IT'S SUNNY!");
+        // System.out.println(str);
+        // str = methods.yellOrWhisper("it's rainy");
+        // System.out.println(str);
 
+        // String str2 = methods.starBetween("banana");
+        // System.out.println(str2);
 
+        boolean str1 = methods.longerThan("pineapple", 10);
+        System.out.println(str1);
 
+        String str2 = methods.funnyString("Paradise", 4);
+        System.out.println(str2);
+
+        String str3 = methods.halvesReversed("Nonsense");
+        System.out.println(str3);
+
+        String str4 = methods.pigLatin("Atlantic");
+        System.out.println(str4);
+
+        String str5 = methods.removeCharacter("bottlecap", 7);
+        System.out.println(str5);
+
+        String str6 = methods.insertAt("Cke", "a", "k");
+        System.out.println(str6);
+
+        String str7 = methods.endUp("Lolipop", 3);
+        System.out.println(str7);
+
+        String str8 = methods.yellOrWhisper("Quiet!");
+        System.out.println(str8);
+
+        String str9 = methods.starBetween("Starlight");
+        System.out.println(str9);
 
     }
 }
